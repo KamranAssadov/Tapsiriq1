83b3f1a44e403e0c13ae3e02fd20129187069c4c
KamranAssadov <115027715+KamranAssadov@users.noreply.github.com>
2022-10-07 20:47:20 +0400

    Add files via upload
    Yazmaga catdirmadim, hemde biraz codun alqoritmi yaddan cixmisdir, ve dunen yenice jupiteri yukledim o sebebden tekrarlamaga erait yox idi((

---
+++ Tapsiriq1.py
@@ -0,0 +1,42 @@
+  print ("""------------
+     Bankomat sistemine xos gelmisiz
+      
+       Prosesler:
+              
+               1- Balans Sorgulama
+               2- Pul Yatirmaq
+               3- Pul Cekmek 
+              
+               -------------------------
+       Proqramdan cixmaq istiyirsinizse "q" ya basin
+       """)
+
+ parol="1357"
+ 
+ istifadeci_parol= input("Parolunuzu daxil ediniz...")
+ while True :
+     parol = int(parol)
+    
+     if(reqem=="q"):
+         print("Proqramdan cixdiniz")
+         break
+
+    while (parol False):
+        print("ValueError")
+        print("Yeniden parolu daxil edin")
+            
+            if (
+                 
+             
+         return 
+
+ elif :
+     print("")
+    
+ elif ():
+     print()
+    
+ else:
+     print
+
+
